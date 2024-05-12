bb027937261dba43cff5431c5324d2c0c62c3d47
Sagar Rajput <sagar.rajput9753@gmail.com>
2024-05-13 00:53:54 +0530

    Navbar and hero section

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+
+import logo from '../../assets/images/logo.png'
+import profileImg from '../../assets/images/avatar-icon.png'
+
+const menuItems = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+  {
+    path: '/doctors',
+    display: 'Find Doctor'
+  },
+  {
+    path: '/contact',
+    display: 'Contact'
+  },
+  {
+    path: '/services',
+    display: 'Services'
+  },
+]
 
 const Header = () => {
+
   return (
-    <div>Header</div>
+    <header className="header flex items-center bg-[#d4d1b8] py-[10px]">
+      <div className="container flex items-center justify-between ">
+        {/* ---------------LOGO---------------------- */}
+        <div>
+          <img src={logo} alt="" />
+        </div>
+
+        {/* ---------------MENU ITEM------------------- */}
+        <ul className="menu text-[16px] flex items-center gap-[2rem]">
+          {
+            menuItems.map((item, ind) => <li key={ind}>
+              <NavLink
+                to={item.path}
+                className={navClass =>
+                  navClass.isActive
+                    ? "text-primaryColor font-[700]"
+                    : "font-[500] hover:text-primaryColor"
+                }
+              >
+                {item.display}
+              </NavLink>
+            </li>)
+          }
+        </ul>
+
+        {/* Profile and login btn */}
+        <div className="flex items-center gap-4">
+          <div className='hidden'>
+            <Link to="/">
+              <img className="w-[37px]" src={profileImg} />
+            </Link>
+          </div>
+
+          <Link to="/login">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-1.5 px-4 rounded">
+              Login
+            </button>
+          </Link>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
+import heroImg from '../assets/images/hero-img01.png'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='hero_sec pt-[60px] 2xl:h-[800px]'>
+        <div className="container">
+          <div className="flex flex-col justify-between items-center w-full lg:flex-row gap-[90px]">
+            {/* hero section */}
+              <div className='lg:w-[570px]'>
+                <h1 className='text-[30px] font-[700] md:text-[60px]'>
+                  Lorem, ipsum dolor sit amet consectetur
+                </h1>
+                <p className='text_para'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, atque in eius pariatur doloremque nemo architecto, numquam sunt quae maxime expedita ducimus nihil deserunt aspernatur suscipit minus illum quas corrupti.
+                </p>
+                <button className='btn'>Book Appointment</button>
+              </div>
+
+            <div className='flex gap-[30px] justify-end'>
+              <div>
+                <img src={heroImg} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
