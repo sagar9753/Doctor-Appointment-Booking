e00932a7e64a881b4b6448a96bfd7724f163ef73
Sagar Rajput <sagar.rajput9753@gmail.com>
2024-06-05 16:52:48 +0530

    Working of updation of doctor profile

--- client/src/pages/Doctor-account/Profile.jsx
+++ client/src/pages/Doctor-account/Profile.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { toast } from 'react-toastify'
 import BounceLoader from 'react-spinners/BeatLoader'
 import { useSelector, useDispatch } from 'react-redux'
+import { MdDeleteOutline } from "react-icons/md";
 
 import { setLogin } from '../../state'
 import uploadImgToCloudinary from '../../utils/uploadCloudinary'
@@ -21,50 +22,118 @@ const Profile = () => {
         gender: user.gender,
         speciality: user.speciality,
         fees: user.fees,
-        bloodType: user.bloodType
+        qualifications: user.qualifications,
+        experiences: user.experiences,
+        timeSlots: user.timeSlots,
+        about: user.about,
+        photo: user.photo
     })
+    const formKeys = Object.keys(formData)
+    const checkUpdatedOrNot = () => {
+        for (let item of formKeys){
+            if( user[item] !== formData[item])
+                return true;
+        };
+        return false;
+    }
 
     const handleForm = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
-        console.log(formData);
     }
 
     const handleFile = async (e) => {
         const file = e.target.files[0]
         const data = await uploadImgToCloudinary(file);
-        console.log(data.url);
 
         setImageURL(data.url);
         setFormData({ ...formData, photo: data.url });
+        console.log(formData);
     }
 
-    const checkUpdatedOrNot = () => {
-        console.log("Check or ");
-        if (user.fullname !== formData.fullname)
-            return true;
-        else if (user.email !== formData.email)
-            return true;
-        else if (user.photo !== formData.photo)
-            return true;
-        else if (user.gender !== formData.gender)
-            return true;
-        else if (user.bloodType !== formData.bloodType)
-            return true;
 
-        return false;
+    // Reusable func for add item
+    const addItem = (key, item) => {
+        setFormData(prevData => ({ ...prevData, [key]: [...prevData[key], item] }))
     }
+
+    // Reusable func for delete item
+    const deleteItem = (key, ind) => {
+        setFormData(prevData => ({
+            ...prevData,
+            [key]: prevData[key].filter((_, i) => i !== ind),
+        }))
+    }
+
+    // Reusable func for input change
+    const handleReusableInputChange = (key, ind, event) => {
+        const { name, value } = event.target
+        console.log(name, value);
+
+        setFormData(prevData => {
+            const updateItems = [...prevData[key]]
+
+            updateItems[ind][name] = value
+
+            return { ...prevData, [key]: updateItems }
+        })
+    }
+
+
+    // handling Qualification operation
+    const addQualification = e => {
+        e.preventDefault();
+
+        addItem("qualifications", { startingDate: "", endingDate: "", degree: "", university: "" })
+    }
+    const deleteQualification = (e, ind) => {
+        e.preventDefault();
+        deleteItem("qualifications", ind);
+    }
+    const handleQualificationChange = (event, ind) => {
+        handleReusableInputChange("qualifications", ind, event);
+    }
+
+    // handling Experience operation
+    const addExperience = e => {
+        e.preventDefault();
+
+        addItem("experiences", { startingDate: "", endingDate: "", position: "", hospital: "" })
+    }
+    const deleteExperience = (e, ind) => {
+        e.preventDefault();
+        deleteItem("experiences", ind);
+    }
+    const handleExperianceChange = (event, ind) => {
+        handleReusableInputChange("experiences", ind, event);
+    }
+
+    // handling TimeSlot operation
+    const addTimeSlot = e => {
+        e.preventDefault();
+
+        addItem("timeSlots", { day: "", startingTime: "", endingTime: "" })
+    }
+    const deleteTimeSlot = (e, ind) => {
+        e.preventDefault();
+        deleteItem("timeSlots", ind);
+    }
+    const handleTimeSlotChange = (event, ind) => {
+        handleReusableInputChange("timeSlots", ind, event);
+    }
+
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         if (!checkUpdatedOrNot()) {
+            console.log("Result",false);
             setLoading(false)
             toast.warning("You did not updated anything");
             return;
         }
 
         try {
-            const res = await fetch(`${import.meta.env.VITE_BACKEND}/users/${user._id}`, {
+            const res = await fetch(`${import.meta.env.VITE_BACKEND}/doctors/${user._id}`, {
                 method: 'put',
                 body: JSON.stringify(formData),
                 headers: {
@@ -91,6 +160,7 @@ const Profile = () => {
             toast.error(err.message)
             setLoading(false)
         }
+
     }
     return (
         <div className='mt-5'>
@@ -116,13 +186,10 @@ const Profile = () => {
                 </div>
                 <div className="mb-5">
                     <p className='text-[15px] font-semibold mb-1'>Bio*</p>
-                    <input type="text" name="bio" placeholder='Bio' value={formData.bio} onChange={handleForm}
+                    <input type="text" name="bio" placeholder='Bio' value={formData.bio} maxLength={100} onChange={handleForm}
                         className='form_input'
                     />
                 </div>
-
-
-
 
                 <div className="mb-5 grid grid-cols-3 gap-5">
                     <label className='text-[#3f3f3f] font-semibold '>
@@ -139,7 +206,7 @@ const Profile = () => {
                     </label>
                     <label className='text-[#3f3f3f] font-semibold '>
                         <p className='text-[15px] font-semibold mb-1'>Speciality*</p>
-                        <select name="gender"
+                        <select name="speciality"
                             value={formData.speciality}
                             onChange={handleForm}
                             className='form_input'
@@ -150,7 +217,7 @@ const Profile = () => {
                             <option value="Dermatologist">Dermatologist</option>
                         </select>
                     </label>
-                    <div className="mb-5">
+                    <div>
                         <p className='text-[15px] font-semibold mb-1'>Fees*</p>
                         <input type="number" name="fees" placeholder='Fees' value={formData.fees} onChange={handleForm}
                             className='form_input'
@@ -159,9 +226,139 @@ const Profile = () => {
 
                 </div>
 
+
+                <div className='mb-10'>
+                    <p className='font-semibold mb-2'>Qualifications*</p>
+                    {formData.qualifications?.map((item, ind) => <div key={ind} className='mb-5' >
+                        <div className='grid grid-cols-2 gap-5 mb-5'>
+                            <div>
+                                <p className='text-[15px] mb-1'>Staring Date*</p>
+                                <input type="date" name="startingDate" value={item.startingDate} className='form_input'
+                                    onChange={e => handleQualificationChange(e, ind)}
+                                />
+                            </div>
+                            <div>
+                                <p className='text-[15px] mb-1'>Ending Date*</p>
+                                <input type="date" name="endingDate" value={item.endingDate} className='form_input'
+                                    onChange={e => handleQualificationChange(e, ind)}
+                                />
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-2 gap-5 mb-5'>
+                            <div>
+                                <p className='text-[15px] mb-1'>Degree*</p>
+                                <input type="text" name="degree" value={item.degree} placeholder='Degree' className='form_input'
+                                    onChange={e => handleQualificationChange(e, ind)}
+                                />
+                            </div>
+                            <div>
+                                <p className='text-[15px] mb-1'>University*</p>
+                                <input type="text" name="university" value={item.university} placeholder='University' className='form_input'
+                                    onChange={e => handleQualificationChange(e, ind)}
+                                />
+                            </div>
+                        </div>
+                        <button onClick={e => deleteQualification(e, ind)} className='p-1 bg-red-500 rounded-full'>
+                            <MdDeleteOutline color='#ffff' size={20} />
+                        </button>
+                    </div>)}
+                    <buton onClick={addQualification} className="btn text-[15px] p-2 cursor-pointer">
+                        Add Qualification
+                    </buton>
+                </div>
+
+                <div className='mb-10'>
+                    <p className='font-semibold mb-2'>Experiences*</p>
+                    {formData.experiences?.map((item, ind) => <div key={ind} className='mb-5' >
+                        <div className='grid grid-cols-2 gap-5 mb-5'>
+                            <div>
+                                <p className='text-[15px] mb-1'>Staring Date*</p>
+                                <input type="date" name="startingDate" value={item.startingDate} className='form_input'
+                                    onChange={e => handleExperianceChange(e, ind)}
+                                />
+                            </div>
+                            <div>
+                                <p className='text-[15px] mb-1'>Ending Date*</p>
+                                <input type="date" name="endingDate" value={item.endingDate} className='form_input'
+                                    onChange={e => handleExperianceChange(e, ind)}
+                                />
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-2 gap-5 mb-5'>
+                            <div>
+                                <p className='text-[15px] mb-1'>Position*</p>
+                                <input type="text" name="position" value={item.position} placeholder='Position' className='form_input'
+                                    onChange={e => handleExperianceChange(e, ind)}
+                                />
+                            </div>
+                            <div>
+                                <p className='text-[15px] mb-1'>Hospital*</p>
+                                <input type="text" name="hospital" value={item.hospital} placeholder='Hospital' className='form_input'
+                                    onChange={e => handleExperianceChange(e, ind)}
+                                />
+                            </div>
+                        </div>
+                        <button onClick={e => deleteExperience(e, ind)} className='p-1 bg-red-500 rounded-full'>
+                            <MdDeleteOutline color='#ffff' size={20} />
+                        </button>
+                    </div>)}
+                    <buton onClick={addExperience} className="btn text-[15px] p-2 cursor-pointer">
+                        Add Experience
+                    </buton>
+                </div>
+
+                <div className='mb-10'>
+                    <p className='font-semibold mb-2'>Time Slots*</p>
+                    {formData.timeSlots?.map((item, ind) => <div key={ind} className='mb-5' >
+                        <div className='grid grid-cols-2 md:grid-cols-4 gap-5 mb-5'>
+                            <div>
+                                <p className='text-[15px] mb-1'>Day*</p>
+                                <select name="day" value={item.day} className='form_input'
+                                    onChange={e => handleTimeSlotChange(e, ind)}
+                                >
+                                    <option value="">Select</option>
+                                    <option value="sunday">Sunday</option>
+                                    <option value="monday">Monday</option>
+                                    <option value="tuesday">Tuesday</option>
+                                    <option value="wednesday">Wednesday</option>
+                                    <option value="thrusday">Thrusday</option>
+                                    <option value="friday">Friday</option>
+                                    <option value="saturday">Saturday</option>
+                                </select>
+                            </div>
+                            <div>
+                                <p className='text-[15px] mb-1'>Starting Time*</p>
+                                <input type="time" name="startingTime" value={item.startingTime} className='form_input'
+                                    onChange={e => handleTimeSlotChange(e, ind)}
+                                />
+                            </div>
+                            <div>
+                                <p className='text-[15px] mb-1'>Ending Time*</p>
+                                <input type="time" name="endingTime" value={item.endingTime} className='form_input'
+                                    onChange={e => handleTimeSlotChange(e, ind)}
+                                />
+                            </div>
+                            <div className='flex items-center'>
+                                <button onClick={e => deleteTimeSlot(e, ind)} className='p-1 bg-red-500 rounded-full mt-7'>
+                                    <MdDeleteOutline color='#ffff' size={20} />
+                                </button>
+                            </div>
+                        </div>
+                    </div>)}
+                    <buton onClick={addTimeSlot} className="btn text-[15px] p-2 cursor-pointer">
+                        Add TimeSlot
+                    </buton>
+                </div>
+
+
+                <div className="mb-5">
+                    <p className='text-[15px] mb-1 font-semibold'>About*</p>
+                    <textarea name="about" value={formData.about} rows={5} placeholder='Write about you' onChange={handleForm} className='form_input' ></textarea>
+                </div>
+
                 <div className=' flex items-center gap-3'>
-                    {user.photo && <div className='rounded-full border-2 flex items-center justify-center '>
-                        <img src={user.photo} alt="" className='w-[50px] h-[50px] rounded-full' />
+                    {formData.photo && <div className='rounded-full border-2 flex items-center justify-center '>
+                        <img src={formData.photo} alt="" className='w-[50px] h-[50px] rounded-full' />
                     </div>}
                     <div>
                         <input type="file" name='photo' id='userPic'
